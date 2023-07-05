2d3a7b9cdd6e2357011f14b5a5d6c3959c8c33b7
Pedro Henrique Prado <pedroh.prado@outlook.com>
2023-07-05 14:11:36 -0300

    upgrade contactController

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -49,6 +49,11 @@ const updateContact = asyncHandler(async (req, res) => {
         res.status(404);
         throw new Error('Contact nor found');
     }
+
+    if (contact.user_id.toString() !== req.user.id) {
+        res.status(403);
+        throw new Error("User don't have permission to update other user contacts");
+    }
     const updateContact = await Contact.findByIdAndUpdate(
         req.params.id,
         req.body,
@@ -65,6 +70,11 @@ const deleteContact = asyncHandler(async (req, res) => {
     if (!contact) {
         res.status(404);
         throw new Error('Contact not found');
+    }
+
+    if (contact.user_id.toString() !== req.user.id) {
+        res.status(403);
+        throw new Error("User don't have permission to update other user contacts");
     }
     //await Contact.remove();
     const deleteContact = await Contact.findByIdAndDelete(contact);
